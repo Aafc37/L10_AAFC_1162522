eb79e6bf208eb610f19a26bec3c82a42eb67c137
aafco <aafco@LAPTOP-9T8JHAEM>
2022-10-14 13:49:20 -0600

    Agregar archivos de proyecto.

---
+++ L10_AAFC_1162522/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string usuario1, contra;
+
+        Console.WriteLine("Bienvenido ;)");
+
+
+     
+
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine("Ingrese un el usuario ");
+            usuario1 = (Console.ReadLine());
+
+
+            Console.WriteLine("Ingrese su contraseña: ");
+            contra = (Console.ReadLine());
+
+
+            if (usuario1 == "usuario" && contra == "asdasd")
+            {
+                Console.WriteLine("Bienvenido, Su usuario y contraseña son correctas ");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Incorrecto");
+            }
+        }
+
+
+
+
+    }
+}
